6699fac32ffaeb8768138b39fc6af3bc05af51ba
NISHCHAY <76655825+Nishchaybhuker20@users.noreply.github.com>
2022-02-07 21:25:34 +0530

    this is the pattern priting question solved by java .

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -20,7 +20,7 @@ public class ButterflyPattern {
                 System.out.print("*");
             }
             int spaces = 2 * (n-i);
-            for(int j = 1; j<= spaces; j++){
+            for(int j = 1; j<=n-i; j++){
                 System.out.print(" ");
             }
             for(int j = 1; j<=i; j++){
