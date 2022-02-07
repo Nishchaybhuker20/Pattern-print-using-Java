dc9d1d1115d163f6a89b04dfbf9fc1e99a2707a5
NISHCHAY <76655825+Nishchaybhuker20@users.noreply.github.com>
2022-02-07 19:20:05 +0530

    this is the pattern priting question solved by java .

---
+++ InvertedHalfPyramidPattern.java
@@ -0,0 +1,15 @@
+public class InvertedHalfPyramidPattern {
+    public static void main(String args[]){
+        int n = 14;
+
+        for(int i = 1; i<n; i++){
+            for(int j =1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ ZeroOneTriangle.java
@@ -0,0 +1,18 @@
+public class ZeroOneTriangle {
+    public static void main(String args[]){
+    int n = 5;
+
+    for(int i = 1; i<=n; i++)
+
+    {
+        for (int j = 1; j <= i; j++) {
+            int sum = i + j;
+            if (sum % 2 == 0) {
+                System.out.print("1");
+            } else {
+                System.out.println("0");
+            }
+        }
+    }
+    }
+}
